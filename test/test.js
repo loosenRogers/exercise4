45e303d869cbfd834dd41d49c3a0a41b2952b974
qianlusen <qianlusen@58ganji.com>
2018-02-23 22:35:05 +0800

    add .travis.yml

--- test/test.js
+++ test/test.js
@@ -1,21 +1,34 @@
 describe('this', function () {
-  it('setTimeout', function (done) {
+  it('setTimeout_with_es6', function (done) {
     var obj = {
       say: function () {
         setTimeout(() => {
+          // 因为用了es6的箭头函数所以会为函数自动绑定this，也就是obj
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj)
           done()
         }, 0)
       }
     }
     obj.say()
-  }) 
+  })
+
+  it('setTimeout_with_es5', function (done) {
+    var obj = {
+      say: function () {
+        var t = setTimeout(function(){
+          this.should.equal(t);
+          done();
+        },0);
+      }
+    }
+    obj.say()
+  })
 
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      this.should.equal(global)
     }
     test()
   })
@@ -26,9 +39,9 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            this.should.equal(global)
           }
-          return _say.bind(obj)
+          return _say.bind(obj)  // obj为undefined   所以bind之后为global
         }()
       }
       obj.say()
@@ -39,11 +52,13 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
       obj.say()
     })
   })
+
+
 })
